44c3b54513e0411c404bb987e0daf29a86640f7b
Matheus Campos da Silva <matheus.campos@mesainc.com.br>
2020-11-08 21:34:29 -0300

    feat: handle map boundaries

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -8,6 +8,8 @@ public class Player : MonoBehaviour
     public static float fuel;
     public Image FuelBar; //barra de combustível que irá diminuir ao longo do uso
     public static bool pressed;
+    public GameObject map;
+    private const float OnMapEndReachedThreshold = 0.1f;
 
     void Start()
     {
@@ -18,12 +20,14 @@ public class Player : MonoBehaviour
 //checa se está sendo pressionado a tela e remove combustível
     void Update()
     {
-      Debug.Log(fuel);
+      // Debug.Log(fuel);
       if (pressed)
       {
         fuel -= 0.001f;
         FuelBar.fillAmount = Mathf.MoveTowards(FuelBar.fillAmount,fuel,Time.deltaTime/10);
       }
+
+        HandleMapBoundaries();
 
     }
     // colisão com gasolina e acrescenta gasolina
@@ -38,5 +42,44 @@ public class Player : MonoBehaviour
         }
         Destroy(col.gameObject);
       }
+    }
+
+    void HandleMapBoundaries()
+    {
+        float mapWidth = map.GetComponent<SpriteRenderer>().bounds.size.x;
+        float mapHeight = map.GetComponent<SpriteRenderer>().bounds.size.y;
+
+        float halfMapWidth = mapWidth / 2;
+        float[] xBoundaries = { -halfMapWidth, halfMapWidth };
+
+        float halfMapHeight = mapHeight / 2;
+        float[] yBoundaries = { -halfMapHeight, halfMapHeight };
+
+        Transform playerTransform = this.gameObject.transform;
+        Vector3 playerTrajectory = new Vector3(
+            playerTransform.position.x * OnMapEndReachedThreshold,
+            playerTransform.position.y * OnMapEndReachedThreshold
+        );
+        Vector3 newPlayerPosition = new Vector3(playerTransform.position.x, playerTransform.position.y);
+
+        if (playerTransform.position.x - playerTrajectory.x <= xBoundaries[0])
+        {
+            newPlayerPosition.x = xBoundaries[1] - 1;
+        }
+        else if (playerTransform.position.x + playerTrajectory.x >= xBoundaries[1])
+        {
+            newPlayerPosition.x = xBoundaries[0] + 1;
+        }
+
+        if (playerTransform.position.y - playerTrajectory.y <= yBoundaries[0])
+        {
+            newPlayerPosition.y = yBoundaries[1] - 1;
+        }
+        else if (playerTransform.position.y + playerTrajectory.y >= yBoundaries[1])
+        {
+            newPlayerPosition.y = yBoundaries[0] + 1;
+        }
+
+        playerTransform.position = newPlayerPosition;
     }
 }
